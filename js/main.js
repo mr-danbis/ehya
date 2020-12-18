8e7f4822f32c18463737bdd489aa367e4b0ce941
Daniel Bisyukov <07.jan.2002@gmail.com>
2020-12-18 13:29:09 +0300

    create: feedback block

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  autoplay: {
+    delay: 7000,
+  },
+})
+
