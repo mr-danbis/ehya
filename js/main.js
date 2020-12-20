ddd2e4b9b410c30a2867bb4510efc4ab305dc91f
Daniel Bisyukov <07.jan.2002@gmail.com>
2020-12-21 02:34:11 +0300

    create: adaptive & distributed media & added favicon

--- js/main.js
+++ js/main.js
@@ -8,11 +8,9 @@ var mySwiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination',
   },
 
-  autoplay: {
-    delay: 7000,
-  },
+
 })
 
-const reviewsContainer = document.querySelector('.feedback-container')
+const reviewsContainer = document.querySelector('.swiper-container')
 reviewsContainer.addEventListener('mouseover', () => mySwiper.autoplay.stop())
 reviewsContainer.addEventListener('mouseout', () => mySwiper.autoplay.start())
